53accc887527bd106a969c34c8635213e47e810c
bberton <bberton@ciandt.com>
2020-04-07 14:58:20 -0300

    Add swagger to project

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace BooksApi
 {
@@ -37,6 +38,11 @@ namespace BooksApi
             services.AddSingleton<BookService>();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Books API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +52,13 @@ namespace BooksApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Books API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             // app.UseHttpsRedirection();
 
